61f88da00087e913446a63c02b4b22802a992bbd
Nguyễn Phan Anh Tuấn <tuan.nguyenphananh@gmail.com>
2024-06-21 12:42:18 +0700

    v12

--- game-sdk.js
+++ game-sdk.js
@@ -1925,7 +1925,7 @@ security_default(window, "GameSDK");
 var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
 
 // <define:__INIT_CONFIG__>
-var define_INIT_CONFIG_default = { BUILD_VERSION: "11", TAGS_TO_CONFIG: ["null"] };
+var define_INIT_CONFIG_default = { BUILD_VERSION: "13", TAGS_TO_CONFIG: ["null"] };
 
 // libs/init-game-sdk.js
 var initConfig = define_INIT_CONFIG_default;
